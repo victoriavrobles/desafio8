130062e0045222d3ec135d835f20fc962e79ebca
victoriavrobles <victoriavrobles@gmail.com>
2022-11-30 21:58:36 -0300

    mysql

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-//Servidor************
 import express from "express"
 import moment from "moment"
 const aplicacion = express();
@@ -18,27 +17,21 @@ const connectionSqlite3 = knex(options.sqlite3)
 const port = 8080;
 const publicRoot = './public';
 
-//Lineas para usar json
 aplicacion.use(express.json());
 aplicacion.use(express.urlencoded({ extended: true }));
 
 // const httpServer = new HttpServer(aplicacion);
 // const io = new IOServer(httpServer);
 
-//***** Hacemos la carpeta public visible
 aplicacion.use(express.static(publicRoot));
-//****************
 
 const productos = new Contenedor(connectionMySql.mysql, 'productos');
 const mensajes = new Contenedor(connectionSqlite3.sqlite3, 'mensajes');
-
-//Endpoints***
 
 aplicacion.get('/', (peticion, respuesta) => {
   respuesta.send('index.html', { root: publicRoot });
 });
 
-//***********
 
 
 //Servidor************
@@ -46,7 +39,6 @@ server.listen(port, () => {
     console.log(`Servidor escuchando http://localhost:${port}`)
 })
 server.on('error', error => console.log(`Error: ${error}`));
-//****************
 
 // MySQL, Sqlite3
 import { mysqlFunc, sqlite3Func } from "./connections/connections.js"
@@ -67,11 +59,9 @@ io.on('connection', async (socket) => {
     io.sockets.emit('producto', data);
   });
 
-  //Para enviar todos los mensajes en la primera conexion
   const listaMensajes = await mensajes.getAll();
   socket.emit('messages', listaMensajes);
 
-  //Evento para recibir nuevos mensajes
   socket.on('new-message', async data => {
     data.time = moment(new Date()).format('DD/MM/YYYY hh:mm:ss');
     await mensajes.save(data);
@@ -79,4 +69,3 @@ io.on('connection', async (socket) => {
     io.sockets.emit('messages', listaMensajes);
   });
 });
-//****************
